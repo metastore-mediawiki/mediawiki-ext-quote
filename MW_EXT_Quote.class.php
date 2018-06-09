84dff4d7aa08a041125ed10fd71b725c1a279e60
Kitsune Solar <kitsune.solar@gmail.com>
2018-06-09 14:21:51 +0300

    Update resource

---
+++ MW_EXT_Quote.class.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Class MW_EXT_Quote
+ * ------------------------------------------------------------------------------------------------------------------ */
+
+class MW_EXT_Quote {
+
+	/**
+	 * Clear DATA (escape html).
+	 *
+	 * @param $string
+	 *
+	 * @return string
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	private static function clearData( $string ) {
+		$outString = htmlspecialchars( trim( $string ), ENT_QUOTES );
+
+		return $outString;
+	}
+
+	/**
+	 * Register tag function.
+	 *
+	 * @param Parser $parser
+	 *
+	 * @return bool
+	 * @throws MWException
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setHook( 'quote', __CLASS__ . '::onRenderTag' );
+
+		return true;
+	}
+
+	/**
+	 * Render tag function.
+	 *
+	 * @param $input
+	 * @param array $args
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 *
+	 * @return string
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public static function onRenderTag( $input, $args = [], Parser $parser, PPFrame $frame ) {
+		// Argument: source.
+		$getSource = self::clearData( $args['source'] ?? '' ?: '' );
+		$outSource = $getSource;
+
+		// Argument: person.
+		$getPerson = self::clearData( $args['person'] ?? '' ?: '' );
+		$outPerson = empty( $getPerson ) ? '' : '<span><i class="far fa-user fa-fw"></i> <a href="' . $outSource . '" target="_blank">' . $getPerson . '</a></span>';
+
+		// Argument: date.
+		$getDate = self::clearData( $args['date'] ?? '' ?: '' );
+		$outDate = empty( $getDate ) ? '' : '<span><i class="far fa-clock fa-fw"></i> ' . $getDate . '</span>';
+
+		// Get content.
+		$getContent = trim( $input );
+		$outContent = $parser->recursiveTagParse( $getContent, $frame );
+
+		// Check person and date arguments, and set footer.
+		if ( $outPerson || $outDate ) {
+			$outFooter = '<footer><cite>' . $outPerson . $outDate . '</cite></footer>';
+		} else {
+			$outFooter = '';
+		}
+
+		// Out HTML.
+		$outHTML = '<blockquote class="mw-ext-quote" cite="' . $outSource . '"><div class="mw-ext-quote-content">' . $outContent . '</div>' . $outFooter . '</blockquote>';
+
+		// Out parser.
+		$outParser = $outHTML;
+
+		return $outParser;
+	}
+
+	/**
+	 * Load resource function.
+	 *
+	 * @param OutputPage $out
+	 * @param Skin $skin
+	 *
+	 * @return bool
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
+		$out->addModuleStyles( array( 'ext.mw.quote.styles' ) );
+
+		return true;
+	}
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Информация
+
+Интеграция цитаты в статью.
+
+## Синтаксис
+
+```
+<quote source="[SOURCE]" date="[DATE]" person="[PERSON]">[CONTENT]</quote>
+```
+
+## Установка
+
+1. Загрузите папки и файлы в директорию `extensions/MW_EXT_Quote`.
+2. В самый низ файла `LocalSettings.php` добавьте строку:
+
+```
+wfLoadExtension( 'MW_EXT_Quote' );
+```
+
+## Авторы
+
+- [**Kitsune Solar**](https://kitsune.solar/) - разработчик.
+
+## Ссылки
+
+- [**METASTORE**](https://metastore.pro/) - хранилище открытых проектов [**METADATA**](https://metadata.foundation/).
+- [**METADATA / Foundation**](https://metadata.foundation/) - поддержка и разработка открытых проектов.
